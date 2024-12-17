a737710d05b01e1965c9963332756b200d3c60bd
bbgatch <10412365+bbgatch@users.noreply.github.com>
2024-12-16 23:30:10 -0500

    Update 3.py

--- 2024/3/3.py
+++ 2024/3/3.py
@@ -1,12 +1,38 @@
 import re
 
-with open('input.txt', 'r') as file:
-    instructions = file.read()
-
-# Find all matching instances of mul(123,123) and put into a list
-matches = re.findall(r"mul\(\d{1,3},\d{1,3}\)", instructions)
-
 # Get just the numbers from each mul(123,123), convert them to integers,
 # multiply, and sum them.
-get_nums_as_ints = [list(map(int, re.findall(r"\d{1,3}", i))) for i in matches]
-sum([i[0] * i[1] for i in get_nums_as_ints])
+def extract_numbers(string):
+    # Find numbers, convert to integers, return as a list
+    return list(map(int, re.findall(r"\d{1,3}", string)))
+
+def part_1(instructions):
+    # Find all matching instances of mul(123,123) and put into a list
+    matches = re.findall(r"mul\(\d{1,3},\d{1,3}\)", instructions)
+    get_nums_as_ints = [extract_numbers(i) for i in matches]
+    print('The answer for Part 1 is ' + str(sum([i[0] * i[1] for i in get_nums_as_ints])) + '🎄.')
+
+def part_2(instructions):
+    # Find all mul(123,123) instances, as well as any instances of do() and don't()
+    matches = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", instructions)
+    results = []
+    include_flag = True
+    for i in matches:
+        if i == "do()":
+            include_flag = True
+        if i == "don't()":
+            include_flag = False        
+        if include_flag == True and i not in ("do()", "don't"):
+            valid_numbers = extract_numbers(i)
+            results.append(valid_numbers[0] * valid_numbers[1])
+
+    print('The answer for Part 2 is ' + str(sum(results)) + '🎁.')
+
+# Read in input and run
+if __name__ == "__main__":
+    # Read in input
+    with open('input.txt', 'r') as file:
+        instructions = file.read()
+    
+    part_1(instructions)
+    part_2(instructions)
